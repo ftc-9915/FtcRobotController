7c4986d4fc155ef996b88942d5724204023670d4
trevorkw7 <>
2021-01-03 16:19:10 -0800

    Update PID Constants

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/DriveConstants.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/DriveConstants.java
@@ -32,7 +32,7 @@ public class DriveConstants {
      * from DriveVelocityPIDTuner.
      */
     public static final boolean RUN_USING_ENCODER = true;
-    public static PIDFCoefficients MOTOR_VELO_PID = new PIDFCoefficients(0, 0, 0,
+    public static PIDFCoefficients MOTOR_VELO_PID = new PIDFCoefficients(11, 0, 10,
             getMotorVelocityF(MAX_RPM / 60 * TICKS_PER_REV));
 
     /*
@@ -64,8 +64,8 @@ public class DriveConstants {
      * small and gradually increase them later after everything is working. All distance units are
      * inches.
      */
-    public static double MAX_VEL = 62.6; // Value given by MaxVelocityTuner: 15.82
-    public static double MAX_ACCEL = 30;
+    public static double MAX_VEL = 62.3; // Value given by MaxVelocityTuner: 62.6
+    public static double MAX_ACCEL = 90;
     public static double MAX_ANG_VEL = Math.toRadians(180);
     public static double MAX_ANG_ACCEL = Math.toRadians(180);
 
