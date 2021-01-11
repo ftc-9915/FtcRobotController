adc838a89a25dbd10754378059feb81eae660347
trevorkw7 <>
2021-01-10 16:08:22 -0800

    Create CoordinateConstants.java
    Constants file for RoadRunner coordinates on the FTC Ultimate Goal field. Add blue alliance starting positions.

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/CoordinateConstants.java
@@ -0,0 +1,12 @@
+package org.firstinspires.ftc.teamcode.drive;
+
+import com.acmerobotics.roadrunner.geometry.Pose2d;
+
+public class CoordinateConstants {
+
+    // Starting positions
+    public static Pose2d START_POS_BLUE_2 = new Pose2d(-60, 26, Math.toRadians(0.0));
+    public static Pose2d START_POS_BLUE_1 = new Pose2d(-60, 50, Math.toRadians(0.0));
+
+
+}
