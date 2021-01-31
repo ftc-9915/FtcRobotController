51829353bca7541e9cce18a7367a4454c9936d2e
Nightmerp <averyz@gmail.com>
2021-01-30 21:32:43 -0800

    Launch Power is Adjustable

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OpModes/TeleOp/TeleOpTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OpModes/TeleOp/TeleOpTest.java
@@ -39,10 +39,11 @@ public class TeleOpTest extends OpMode {
     // Ensures that the adjustments are made each time the gamepad buttons are pressed rather than each time through loop
     boolean buttonReleased1;
     boolean buttonReleased2;
+    boolean triggerReleased;
 
     static final double LIFT_UP_POS = 0.49 ;
     static final double LIFT_DOWN_POS = 0.75;
-    static final double NOT_PUSH_POS = 0.68;
+    static final double NOT_PUSH_POS = 0.70;
     static final double PUSH_POS = 0.52;
 
     // TODO: test these and edit with accurate values
@@ -56,6 +57,7 @@ public class TeleOpTest extends OpMode {
     double speed = 0.0;
     double strafe = 0.0;
     double rotation = 0.0;
+    double strafePower = 1.0;
 
     @Override
     public void init() {
@@ -96,6 +98,7 @@ public class TeleOpTest extends OpMode {
         wobbleArmPos = 0;
         buttonReleased1 = true;
         buttonReleased2 = true;
+        triggerReleased = true;
 
         // Starting position
         liftServo.setPosition(LIFT_DOWN_POS);
@@ -106,6 +109,7 @@ public class TeleOpTest extends OpMode {
     @Override
     public void loop() {
         // Controls and Information
+        telemetry.addData("Launcher Speed: ", launcherPower);
         telemetry.addLine("--- Controls (Gamepad 1) ---");
         telemetry.addData("Turn collector on", "Button A");
         telemetry.addData("Turn collector off", "Button B");
@@ -118,9 +122,9 @@ public class TeleOpTest extends OpMode {
         telemetry.addData("Lift collector platform", "Right Bumper");
 
         // Chassis code
-        speed = -gamepad1.right_stick_y;
-        strafe = gamepad1.right_stick_x;
-        rotation = gamepad1.left_stick_x;
+        speed = -gamepad1.left_stick_y * strafePower;
+        strafe = gamepad1.left_stick_x * strafePower;
+        rotation = gamepad1.right_stick_x * strafePower;
 
 
         leftFront.setPower(speed + strafe + rotation);
@@ -128,11 +132,22 @@ public class TeleOpTest extends OpMode {
         rightBack.setPower(speed + strafe - rotation);
         rightFront.setPower(speed - strafe - rotation);
 
+        // Slowmode
 
+        if (gamepad1.y && buttonReleased1) {
+            if (strafePower == 1.0) {
+                strafePower = 0.5;
+            } else {
+                strafePower = 1.0;
+            }
+            buttonReleased1 = false;
+        }
 
         // Turns collector on/off
         if (gamepad1.a && buttonReleased1) {
             collectorPower = 1.0;
+            pushServo.setPosition(NOT_PUSH_POS);
+            liftServo.setPosition(LIFT_DOWN_POS);
             buttonReleased1 = false;
         }
 
@@ -151,11 +166,23 @@ public class TeleOpTest extends OpMode {
         // Turns launcher on/off
         if (gamepad2.x && buttonReleased2) {
             if (launcherPower == 0.0) {
-                launcherPower = -1.0;
+                launcherPower = -0.68;
             } else {
                 launcherPower = 0.0;
             }
             buttonReleased2 = false;
+        }
+
+        // Adjusts launcher speed every time trigger goes below 0.4
+
+        if (gamepad2.left_trigger > 0.4 && triggerReleased) {
+            launcherPower -= 0.05;
+            triggerReleased = false;
+        }
+
+        if (gamepad2.right_trigger > 0.4 && triggerReleased) {
+            launcherPower += 0.05;
+            triggerReleased = false;
         }
 
         // Pushes/retracts collector servo
@@ -209,12 +236,16 @@ public class TeleOpTest extends OpMode {
 
         // Do not adjust values again until after buttons are released (and pressed again) so the
         // adjustments are made each time the gamepad buttons are pressed rather than each time through loop
-        if (!gamepad1.a && !gamepad1.b && !gamepad1.x) {
+        if (!gamepad1.a && !gamepad1.b && !gamepad1.x && !gamepad1.y) {
             buttonReleased1 = true;
         }
 
         if(!gamepad2.left_bumper && !gamepad2.right_bumper && !gamepad2.a && !gamepad2.b && !gamepad2.x && !gamepad2.y) {
             buttonReleased2 = true;
+        }
+
+        if (gamepad2.left_trigger < 0.2 && gamepad2.right_trigger < 0.2) {
+            triggerReleased = true;
         }
 
         launcherMotor.setPower(launcherPower);
