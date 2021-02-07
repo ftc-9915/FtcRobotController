f095a27ae1ec8dd8640181b2723047f375324018
Nightmerp <averyz@gmail.com>
2021-02-07 15:53:53 -0800

    Update TeleOp

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OpModes/PoseStorage.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OpModes/PoseStorage.java
@@ -3,5 +3,5 @@ package org.firstinspires.ftc.teamcode.OpModes;
 import com.acmerobotics.roadrunner.geometry.Pose2d;
 
 public class PoseStorage {
-    public static Pose2d autoEndingPose = new Pose2d();
+    public static Pose2d autoEndingPose = new Pose2d(72, 60, 0);
 }
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OpModes/TeleOp/TeleOpTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OpModes/TeleOp/TeleOpTest.java
@@ -7,10 +7,12 @@ import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.Servo;
+import com.qualcomm.robotcore.util.ElapsedTime;
 
 import org.firstinspires.ftc.teamcode.OpModes.PoseStorage;
 import org.firstinspires.ftc.teamcode.Subsystems.Drive.SampleMecanumDrive;
 import org.firstinspires.ftc.teamcode.Subsystems.Drive.StandardTrackingWheelLocalizer;
+import org.firstinspires.ftc.teamcode.Subsystems.Shooter.Shooter;
 
 
 @Config
@@ -24,26 +26,23 @@ public class TeleOpTest extends OpMode {
 
 
 
-    DcMotor launcherMotor;
+    //DcMotor launcherMotor;
 
     DcMotor collectorMotor;
 
-    DcMotor wobbleMotor;
+    DcMotor armMotor;
 
     Servo liftServo;
     Servo pushServo;
-    Servo wobbleServo;
+    Servo clawServo;
 
-    //Servo feederServo;
-
-    // TODO: test these and edit with accurate values
-    static final double SERVO_RESET_POS = 1.0;
-    static final double SERVO_PUSH_POS = 0.0;
+    ElapsedTime timer = new ElapsedTime();
 
     double launcherPower;
+    double launcherRPM;
+    boolean launcherOn;
     double collectorPower;
-    boolean collectorPush;
-    int wobbleArmPos;
+    int armPos;
 
     // Ensures that the adjustments are made each time the gamepad buttons are pressed rather than each time through loop
     boolean buttonReleased1;
@@ -56,11 +55,11 @@ public class TeleOpTest extends OpMode {
     static final double PUSH_POS = 0.52;
 
     // TODO: test these and edit with accurate values
-    static final int WOBBLE_ARM_SPEED = 10;
-    static final int WOBBLE_ARM_UPPER_LIMIT = 1000;
-    static final int WOBBLE_ARM_LOWER_LIMIT = 0;
-    static final double WOBBLE_CLAW_OPEN = 1.0;
-    static final double WOBBLE_CLAW_CLOSE = 0.0;
+    static final int ARM_SPEED = 2;
+    static final int ARM_UPPER_LIMIT = 10000;
+    static final int ARM_LOWER_LIMIT = -10000;
+    static final double CLAW_OPEN_POS = 0.7;
+    static final double CLAW_CLOSE_POS = 0.15;
 
 
     double speed = 0.0;
@@ -68,6 +67,8 @@ public class TeleOpTest extends OpMode {
     double rotation = 0.0;
     double strafePower = 1.0;
     boolean slowmodeOn = false;
+
+    Shooter shooter;
 
     SampleMecanumDrive drive;
     public static int shootingPositionX;
@@ -95,30 +96,28 @@ public class TeleOpTest extends OpMode {
 
 
         // Attachment Motors
-        launcherMotor = hardwareMap.dcMotor.get("launcherMotor");
-        launcherMotor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        //launcherMotor = hardwareMap.dcMotor.get("launcherMotor");
+        //launcherMotor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
         collectorMotor = hardwareMap.dcMotor.get("collectorMotor");
-        launcherMotor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        //launcherMotor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
 
         liftServo = hardwareMap.servo.get("liftServo");
         pushServo = hardwareMap.servo.get("pushServo");
 
-        /*
-        wobbleMotor = hardwareMap.dcMotor.get("wobbleMotor");
-        wobbleMotor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-        wobbleMotor.setTargetPosition(0);
-        wobbleMotor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-        wobbleServo = hardwareMap.servo.get("wobbleServo");
-         */
 
-        //feederServo = hardwareMap.servo.get("feederServo");
-        //feederServo.setPosition(SERVO_RESET_POS);
+        armMotor = hardwareMap.dcMotor.get("armMotor");
+        armMotor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        armMotor.setTargetPosition(0);
+        armMotor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        clawServo = hardwareMap.servo.get("clawServo");
+
 
         // Initialization values
         launcherPower = 0.0;
+        launcherRPM = -60.0;
+        launcherOn = false;
         collectorPower = 1.0;
-        collectorPush = false;
-        wobbleArmPos = 0;
+        armPos = 0;
         buttonReleased1 = true;
         buttonReleased2 = true;
         triggerReleased = true;
@@ -139,18 +138,17 @@ public class TeleOpTest extends OpMode {
     public void loop() {
         drive.update();
 
-
-        // Controls and Information
-
         // Retrieve pose
         Pose2d currentPose = drive.getPoseEstimate();
 
+        // Controls and Information
         telemetry.addData("Drive Mode: ", currentMode);
         telemetry.addData("x", currentPose.getX());
         telemetry.addData("y", currentPose.getY());
         telemetry.addData("heading", currentPose.getHeading());
 
-        telemetry.addData("Launcher Speed", launcherPower);
+        telemetry.addData("Launcher RPM", shooter.getRPM());
+        telemetry.addData("Arm Position", armPos);
         telemetry.addData("Slowmode On", slowmodeOn);
         telemetry.addLine("--- Controls (Gamepad 1) ---");
         telemetry.addData("Turn collector on", "Button A");
@@ -162,6 +160,9 @@ public class TeleOpTest extends OpMode {
 
         telemetry.addLine();
         telemetry.addLine("--- Controls (Gamepad 2) ---");
+        telemetry.addData("Open Claw", "Button A");
+        telemetry.addData("Close Claw", "Button B");
+        telemetry.addData("Move Arm", "Left Stick Up/Down");
         telemetry.addData("Turn launcher on/off", "Button X");
         telemetry.addData("Push/retract collector servo", "Button Y");
         telemetry.addData("Lower collector platform", "Left Bumper");
@@ -218,35 +219,45 @@ public class TeleOpTest extends OpMode {
 
                 // Turns launcher on/off
                 if (gamepad2.x && buttonReleased2) {
+                    /*
                     if (launcherPower == 0.0) {
                         launcherPower = -0.68;
                     } else {
                         launcherPower = 0.0;
                     }
+                     */
+                    if (launcherOn) {
+                        shooter.setRPM(0);
+                    } else {
+                        shooter.setRPM(launcherRPM);
+                    }
                     buttonReleased2 = false;
                 }
 
                 // Adjusts launcher speed every time trigger goes below 0.4
-                if (gamepad2.left_trigger > 0.4 && triggerReleased) {
-                    launcherPower += 0.05;
+                if (gamepad2.left_trigger > 0.4 && triggerReleased && launcherOn) {
+                    //launcherPower += 0.05;
+                    launcherRPM += 5;
+                    shooter.setRPM(launcherRPM);
                     triggerReleased = false;
                 }
 
-                if (gamepad2.right_trigger > 0.4 && triggerReleased) {
-                    launcherPower -= 0.05;
+                if (gamepad2.right_trigger > 0.4 && triggerReleased && launcherOn) {
+                    //launcherPower -= 0.05;
+                    launcherRPM -= 5;
+                    shooter.setRPM(launcherRPM);
                     triggerReleased = false;
                 }
 
                 // Pushes/retracts collector servo
                 if (gamepad2.y && buttonReleased2) {
-                    if (collectorPush) {
-                        pushServo.setPosition(NOT_PUSH_POS);
-                        collectorPush = false;
-                    } else {
-                        pushServo.setPosition(PUSH_POS);
-                        collectorPush = true;
-                    }
+                    pushServo.setPosition(PUSH_POS);
+                    timer.reset();
                     buttonReleased2 = false;
+                }
+
+                if (timer.seconds() > 1) {
+                    pushServo.setPosition(NOT_PUSH_POS);
                 }
 
                 // Lifts/Lowers the collecting platform
@@ -260,30 +271,30 @@ public class TeleOpTest extends OpMode {
                     buttonReleased2 = false;
                 }
 
-                        // Lifts/Lowers Wobble Goal Arm
-                /*
-                wobbleArmPos += WOBBLE_ARM_SPEED * gamepad2.left_stick_y;
-                if (wobbleArmPos > WOBBLE_ARM_UPPER_LIMIT) {
-                    wobbleArmPos = WOBBLE_ARM_UPPER_LIMIT;
-                }
-                if (wobbleArmPos < WOBBLE_ARM_LOWER_LIMIT) {
-                    wobbleArmPos = WOBBLE_ARM_LOWER_LIMIT;
-                }
-                wobbleMotor.setTargetPosition(wobbleArmPos);
-                wobbleMotor.setPower(1.0);
-                 */
+                // Lifts/Lowers Wobble Goal Arm
 
-                        // Opens/Closes Wobble Goal Claw
-                /*
+                armPos += ARM_SPEED * gamepad2.left_stick_y;
+                if (armPos > ARM_UPPER_LIMIT) {
+                    armPos = ARM_UPPER_LIMIT;
+                }
+                if (armPos < ARM_LOWER_LIMIT) {
+                    armPos = ARM_LOWER_LIMIT;
+                }
+                armMotor.setTargetPosition(armPos);
+                armMotor.setPower(1.0);
+
+
+                // Opens/Closes Wobble Goal Claw
+
                 if (gamepad2.a && buttonReleased2) {
-                    wobbleServo.setPosition(WOBBLE_CLAW_OPEN);
+                    clawServo.setPosition(CLAW_OPEN_POS);
                     buttonReleased2 = false;
                 }
                 if (gamepad2.b && buttonReleased2) {
-                    wobbleServo.setPosition(WOBBLE_CLAW_CLOSED);
+                    clawServo.setPosition(CLAW_CLOSE_POS);
                     buttonReleased2 = false;
                 }
-                 */
+
 
 
                 // Do not adjust values again until after buttons are released (and pressed again) so the
@@ -300,15 +311,17 @@ public class TeleOpTest extends OpMode {
                     triggerReleased = true;
                 }
 
-                launcherMotor.setPower(launcherPower);
+                //launcherMotor.setPower(launcherPower);
                 collectorMotor.setPower(collectorPower);
 
 
                 //create trajectory to shooting position on the fly
                 if (gamepad1.dpad_up) {
-                    // If the A button is pressed on gamepad1, we generate a splineTo()
+                    // If the D-pad up button is pressed on gamepad1, we generate a splineTo()
                     // trajectory on the fly and follow it
                     // We switch the state to AUTOMATIC_CONTROL
+
+                    liftServo.setPosition(LIFT_UP_POS);
 
                     Trajectory driveToShootPositionPath = drive.trajectoryBuilder(currentPose)
                             .lineToLinearHeading(new Pose2d(shootingPositionX, shootingPositionY,  Math.toRadians(shootingPositionHeading)))
