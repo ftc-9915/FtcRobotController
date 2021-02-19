a130f511c631425235fc7c0d6951cb624d2aa642
Trevor Kwan <trevorkw7@gmail.com>
2021-02-18 23:30:17 -0800

    Update AutonomousPathBAsync.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OpModes/Autonomous/AutonomousPathBAsync.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OpModes/Autonomous/AutonomousPathBAsync.java
@@ -7,10 +7,9 @@ import com.acmerobotics.roadrunner.trajectory.constraints.AngularVelocityConstra
 import com.acmerobotics.roadrunner.trajectory.constraints.MecanumVelocityConstraint;
 import com.acmerobotics.roadrunner.trajectory.constraints.MinVelocityConstraint;
 import com.acmerobotics.roadrunner.trajectory.constraints.ProfileAccelerationConstraint;
-import com.acmerobotics.roadrunner.util.NanoClock;
+import com.qualcomm.robotcore.util.ElapsedTime;
 
 import org.firstinspires.ftc.robotcore.external.Telemetry;
-import org.firstinspires.ftc.teamcode.Commands.ShootCommand;
 import org.firstinspires.ftc.teamcode.Common.UtilMethods;
 import org.firstinspires.ftc.teamcode.Subsystems.Collector;
 import org.firstinspires.ftc.teamcode.Subsystems.Drive.DriveConstants;
@@ -19,14 +18,18 @@ import org.firstinspires.ftc.teamcode.Subsystems.Drive.PoseLibrary;
 import org.firstinspires.ftc.teamcode.Subsystems.Shooter.Flywheel;
 import org.firstinspires.ftc.teamcode.Subsystems.Shooter.Hopper;
 import org.firstinspires.ftc.teamcode.Subsystems.WobbleArm;
-import com.qualcomm.robotcore.util.ElapsedTime;
-
 
 import java.util.Arrays;
 
 @Config
 public class AutonomousPathBAsync extends AutonomousPathAsync {
 
+    private static final double RPM_FORGIVENESS = 200;
+    //Treakable values for tuning
+    public static int goalX = -30;
+    public static int goalY = 59;
+    public static double shootingPoseAngle = 7;
+    public static double shootingPoseRPM = 3350;
     //Pre declare trajectories
     Trajectory goToShootingPose;
     Trajectory goToPlaceGoalPose;
@@ -35,19 +38,11 @@ public class AutonomousPathBAsync extends AutonomousPathAsync {
     Trajectory goToPlaceSecondGoalPart1;
     Trajectory goToPlaceSecondGoalPart2;
     Trajectory goToParkingPose;
-
-    //Treakable values for tuning
-    public static int goalX = -30;
-    public static int goalY = 59;
-    public static double shootingPoseAngle = 7;
-    public static double shootingPoseRPM = 3350;
-    private static final double RPM_FORGIVENESS = 200;
-
-
     //Set starting state and rings to shoot and timer
     State currentState = State.DRIVE_TO_SHOOT;
     int rings = 3;
     ElapsedTime timer = new ElapsedTime();
+    boolean hopperPositionIn = false;
 
     //Poses
     Pose2d shootingPose = new Pose2d(6.8066, 26.37388, Math.toRadians(shootingPoseAngle));
@@ -57,21 +52,6 @@ public class AutonomousPathBAsync extends AutonomousPathAsync {
     Pose2d placeSecondGoalPose1 = new Pose2d(27, 57, Math.toRadians(0.0));
     Pose2d placeSecondGoalPose2 = new Pose2d(20, 30, Math.toRadians(0.0));
     Pose2d parkPose = new Pose2d(12, 27, Math.toRadians(0.0));
-
-    // This enum defines our "state"
-    // This is essentially just defines the possible steps our program will take
-    enum State {
-        DRIVE_TO_SHOOT,
-        SHOOT,   // First, follow a splineTo() trajectory
-        DRIVE_TO_PLACE_GOAL,   // Then, follow a lineTo() trajectory
-        PLACE_GOAL,         // Then we want to do a point turn
-        DRIVE_TO_SECOND_GOAL,   // Then, we follow another lineTo() trajectory
-        PICKUP_SECOND_GOAL,         // Then we're gonna wait a second
-        DRIVE_TO_PLACE_SECOND_GOAL,         // Finally, we're gonna turn again
-        PLACE_SECOND_GOAL,
-        PARK,
-        IDLE// Our bot will enter the IDLE state when done
-    }
 
     //build trajectories on construction
     public AutonomousPathBAsync(MecanumDrivebase drive, WobbleArm wobbleArm, Flywheel flywheel, Collector collector, Hopper hopper) {
@@ -86,12 +66,12 @@ public class AutonomousPathBAsync extends AutonomousPathAsync {
                 .splineTo(placeGoalPose.vec(), placeGoalPose.getHeading())
                 .build();
 
-         goToPickUpGoalPose1 = drive.trajectoryBuilder(goToPlaceGoalPose.end())
+        goToPickUpGoalPose1 = drive.trajectoryBuilder(goToPlaceGoalPose.end())
                 .lineToLinearHeading(pickUpGoalPose1)
                 .addDisplacementMarker(() -> drive.followTrajectoryAsync(goToPickUpGoalPose2))
                 .build();
 
-         goToPickUpGoalPose2 = drive.trajectoryBuilder(goToPickUpGoalPose1.end())
+        goToPickUpGoalPose2 = drive.trajectoryBuilder(goToPickUpGoalPose1.end())
                 .lineToConstantHeading(pickUpGoalPose2.vec(), new MinVelocityConstraint(
                                 Arrays.asList(
                                         new AngularVelocityConstraint(DriveConstants.MAX_ANG_VEL),
@@ -102,22 +82,21 @@ public class AutonomousPathBAsync extends AutonomousPathAsync {
                 .build();
 
 
-         goToPlaceSecondGoalPart1 = drive.trajectoryBuilder(goToPickUpGoalPose2.end())
+        goToPlaceSecondGoalPart1 = drive.trajectoryBuilder(goToPickUpGoalPose2.end())
                 .lineToSplineHeading(placeSecondGoalPose1)
-                 .addDisplacementMarker(() -> {
-                     drive.followTrajectoryAsync(goToPlaceSecondGoalPart2);
-                 })
+                .addDisplacementMarker(() -> {
+                    drive.followTrajectoryAsync(goToPlaceSecondGoalPart2);
+                })
                 .build();
 
-         goToPlaceSecondGoalPart2 = drive.trajectoryBuilder(goToPlaceSecondGoalPart1.end())
+        goToPlaceSecondGoalPart2 = drive.trajectoryBuilder(goToPlaceSecondGoalPart1.end())
                 .lineToConstantHeading(placeSecondGoalPose2.vec())
                 .build();
 
-         goToParkingPose = drive.trajectoryBuilder(goToPlaceSecondGoalPart2.end())
+        goToParkingPose = drive.trajectoryBuilder(goToPlaceSecondGoalPart2.end())
                 .lineToConstantHeading(parkPose.vec())
                 .build();
     }
-
 
     @Override
     public void followPathAsync(Telemetry telemetry) {
@@ -135,18 +114,25 @@ public class AutonomousPathBAsync extends AutonomousPathAsync {
                 }
                 break;
             case SHOOT:
-                flywheel.setRPM(shootingPoseRPM);
-                hopper.setLiftUpPos();
-                if (rings > 0){
-                    if (UtilMethods.inRange(shootingPoseRPM, shootingPoseRPM - RPM_FORGIVENESS, shootingPoseRPM + RPM_FORGIVENESS)) {
-                        hopper.setPushInPos();
-                        UtilMethods.sleep(500);
-                        hopper.setPushOutPos();
-                        rings--;
+                if (!drive.isBusy()) {
+                    flywheel.setRPM(shootingPoseRPM);
+                    hopper.setLiftUpPos();
+                    if (rings > 0) {
+                        if (UtilMethods.inRange(shootingPoseRPM, shootingPoseRPM - RPM_FORGIVENESS, shootingPoseRPM + RPM_FORGIVENESS) && !hopperPositionIn) {
+                            hopper.setPushInPos();
+                            timer.reset();
+                            hopperPositionIn = true;
+                        }
+                        if (hopperPositionIn && timer.seconds() > 0.5) {
+                            hopper.setPushOutPos();
+                            hopperPositionIn = false;
+                            rings--;
+                        }
                     }
-                } else {
-                    currentState = State.DRIVE_TO_PLACE_GOAL;
-                    drive.followTrajectoryAsync(goToPlaceGoalPose);
+                    else {
+                        currentState = State.DRIVE_TO_PLACE_GOAL;
+                        drive.followTrajectoryAsync(goToPlaceGoalPose);
+                    }
                 }
                 break;
 
@@ -156,7 +142,7 @@ public class AutonomousPathBAsync extends AutonomousPathAsync {
                     timer.reset();
                 }
                 break;
-                
+
             case PLACE_GOAL:
                 //Trigger action depending on timer using else if logic
                 if (timer.seconds() > 2) {
@@ -164,11 +150,9 @@ public class AutonomousPathBAsync extends AutonomousPathAsync {
                     wobbleArm.liftArm();
                     //will drive to pose 1 and pose 2 using displacement marker
                     drive.followTrajectoryAsync(goToPickUpGoalPose1);
-                }
-                else if (timer.seconds() > 1) {
+                } else if (timer.seconds() > 1) {
                     wobbleArm.openClaw();
-                }
-                else if (timer.seconds() > 0.5) {
+                } else if (timer.seconds() > 0.5) {
                     wobbleArm.lowerArm();
                 }
                 break;
@@ -185,11 +169,9 @@ public class AutonomousPathBAsync extends AutonomousPathAsync {
                     wobbleArm.liftArm();
                     //will drive to pose 1 and pose 2 using displacement marker
                     drive.followTrajectoryAsync(goToPlaceSecondGoalPart1);
-                }
-                else if (timer.seconds() > 1) {
+                } else if (timer.seconds() > 1) {
                     wobbleArm.closeClaw();
-                }
-                else if (timer.seconds() > 0.5) {
+                } else if (timer.seconds() > 0.5) {
                     wobbleArm.lowerArm();
                 }
                 break;
@@ -206,11 +188,9 @@ public class AutonomousPathBAsync extends AutonomousPathAsync {
                     wobbleArm.liftArm();
                     //will drive to pose 1 and pose 2 using displacement marker
                     drive.followTrajectoryAsync(goToParkingPose);
-                }
-                else if (timer.seconds() > 1) {
+                } else if (timer.seconds() > 1) {
                     wobbleArm.openClaw();
-                }
-                else if (timer.seconds() > 0.5) {
+                } else if (timer.seconds() > 0.5) {
                     wobbleArm.lowerArm();
                 }
                 break;
@@ -241,6 +221,22 @@ public class AutonomousPathBAsync extends AutonomousPathAsync {
         telemetry.addData("Current State", currentState);
         telemetry.update();
 
+    }
+
+
+    // This enum defines our "state"
+    // This is essentially just defines the possible steps our program will take
+    enum State {
+        DRIVE_TO_SHOOT,
+        SHOOT,   // First, follow a splineTo() trajectory
+        DRIVE_TO_PLACE_GOAL,   // Then, follow a lineTo() trajectory
+        PLACE_GOAL,         // Then we want to do a point turn
+        DRIVE_TO_SECOND_GOAL,   // Then, we follow another lineTo() trajectory
+        PICKUP_SECOND_GOAL,         // Then we're gonna wait a second
+        DRIVE_TO_PLACE_SECOND_GOAL,         // Finally, we're gonna turn again
+        PLACE_SECOND_GOAL,
+        PARK,
+        IDLE// Our bot will enter the IDLE state when done
     }
 
 }
