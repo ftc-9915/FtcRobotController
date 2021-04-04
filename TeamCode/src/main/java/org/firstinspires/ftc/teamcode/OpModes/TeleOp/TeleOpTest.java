574034c0695993051e13a7009e01129e07792f61
Trevor Kwan <trevorkw7@gmail.com>
2021-04-03 19:06:22 -0700

    Retain controls during auto modes in teleop

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OpModes/TeleOp/TeleOpTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OpModes/TeleOp/TeleOpTest.java
@@ -210,224 +210,224 @@ public class TeleOpTest extends OpMode {
         telemetry.addData("Increase Launcher Speed", "Right Trigger");
          */
 
-        switch (currentMode){
-            case DRIVER_CONTROL:
 
-                // Chassis code
-                speed = -gamepad1.left_stick_y * strafePower;
-                strafe = gamepad1.left_stick_x * strafePower;
-                rotation = gamepad1.right_stick_x * strafePower;
-
-
-                drive.setMotorPowers(speed + strafe + rotation, speed - strafe + rotation, speed + strafe - rotation, speed - strafe - rotation);
-
-                // Slowmode
-                if (gamepad1.y && buttonReleased1) {
-                    if (slowmodeOn) {
-
-                        strafePower = 1.0;
-                        slowmodeOn = false;
-                    } else {
-
-                        strafePower = 0.5;
-                        slowmodeOn = true;
-                    }
-                    buttonReleased1 = false;
-                }
-
-                // Turns collector on/off
-                if (gamepad1.a && buttonReleased1) {
-                    collector.turnCollectorOn();
-                    hopper.setPushOutPos();
-                    hopper.setLiftDownPos();
-                    buttonReleased1 = false;
-                }
-
-                if (gamepad1.b && buttonReleased1) {
-                    collector.turnCollectorOff();
-                    buttonReleased1 = false;
-                }
-
-                // Reverses collector
-                if (gamepad1.x && buttonReleased1) {
-                    collector.turnCollectorReverse();
-                    buttonReleased1 = false;
-                }
-
-                // Turns launcher on/off
-                if (gamepad2.x && buttonReleased2) {
-                    if (launcherOn) {
-                        flywheel.setRPM(0);
-                        launcherOn = false;
-                    } else {
-                        flywheel.setRPM(launcherRPM);
-                        launcherOn = true;
-                    }
-                    buttonReleased2 = false;
-                }
-
-                // Adjusts launcher speed every time trigger goes below 0.4
-                if (gamepad2.left_trigger > 0.4 && triggerReleased && launcherOn) {
-                    launcherRPM -= 50;
-                    flywheel.setRPM(launcherRPM);
-                    triggerReleased = false;
-                }
-
-                if (gamepad2.right_trigger > 0.4 && triggerReleased && launcherOn) {
-                    launcherRPM += 50;
-                    flywheel.setRPM(launcherRPM);
-                    triggerReleased = false;
-                }
-
-                // Pushes/retracts collector servo
-                if (gamepad2.y && buttonReleased2) {
-                    hopper.setPushInPos();
-                    timer.reset();
-                    buttonReleased2 = false;
-                }
-
-                if (timer.seconds() > 0.75) {
-                    hopper.setPushOutPos();
-                }
-
-                // Lifts/Lowers the collecting platform
-                if (gamepad2.left_bumper && buttonReleased2) {
-                    hopper.setLiftDownPos();
-                    buttonReleased2 = false;
-                }
-
-                if (gamepad2.right_bumper && buttonReleased2) {
-                    collector.turnCollectorOff();
-                    hopper.setLiftUpPos();
-                    buttonReleased2 = false;
-                }
-
-                // Lifts/Lowers Wobble Goal Arm
-                armPos -= 10 * gamepad2.left_stick_y;
-                if (armPos < wobbleArm.ARM_LOWER_LIMIT) {
-                    armPos = wobbleArm.ARM_LOWER_LIMIT;
-                }
-                if (armPos > wobbleArm.ARM_UPPER_LIMIT) {
-                    armPos = wobbleArm.ARM_UPPER_LIMIT;
-                }
-
-                // Arm Presets
-                if (gamepad2.dpad_up && buttonReleased2) {
-                    armPos = wobbleArm.ARM_POS_LIFT_ARM;
-                    buttonReleased2 = false;
-                }
-                if (gamepad2.dpad_down && buttonReleased2) {
-                    armPos = wobbleArm.ARM_POS_PICKUP_GOAL;
-                    wobbleArm.openClaw();
-                    buttonReleased2 = false;
-                }
-                if (gamepad2.dpad_right && buttonReleased2) {
-                    armPos = wobbleArm.ARM_POS_OVER_WALL;
-                    buttonReleased2 = false;
-                }
-                if (gamepad2.dpad_left && buttonReleased2) {
-                    armPos = 0;
-                    buttonReleased2 = false;
-                }
-                wobbleArm.setArmPos(armPos);
-
-                // Opens/Closes Wobble Goal Claw
-                if (gamepad2.a && buttonReleased2) {
-                    wobbleArm.openClaw();
-                    buttonReleased2 = false;
-                }
-                if (gamepad2.b && buttonReleased2) {
-                    wobbleArm.closeClaw();
-                    buttonReleased2 = false;
-                }
+        //driver controls
+        // Chassis code
+        speed = -gamepad1.left_stick_y * strafePower;
+        strafe = gamepad1.left_stick_x * strafePower;
+        rotation = gamepad1.right_stick_x * strafePower;
 
 
+        drive.setMotorPowers(speed + strafe + rotation, speed - strafe + rotation, speed + strafe - rotation, speed - strafe - rotation);
 
-                // Do not adjust values again until after buttons are released (and pressed again) so the
-                // adjustments are made each time the gamepad buttons are pressed rather than each time through loop
-                if (!gamepad1.a && !gamepad1.b && !gamepad1.x && !gamepad1.y) {
-                    buttonReleased1 = true;
-                }
+        // Slowmode
+        if (gamepad1.y && buttonReleased1) {
+            if (slowmodeOn) {
 
-                if(!gamepad2.left_bumper && !gamepad2.right_bumper && !gamepad2.a && !gamepad2.b && !gamepad2.x && !gamepad2.y && !gamepad2.dpad_up && !gamepad2.dpad_right && !gamepad2.dpad_down && !gamepad2.dpad_left) {
-                    buttonReleased2 = true;
-                }
+                strafePower = 1.0;
+                slowmodeOn = false;
+            } else {
 
-                if (gamepad2.left_trigger < 0.4 && gamepad2.right_trigger < 0.4) {
-                    triggerReleased = true;
-                }
+                strafePower = 0.5;
+                slowmodeOn = true;
+            }
+            buttonReleased1 = false;
+        }
+
+        // Turns collector on/off
+        if (gamepad1.a && buttonReleased1) {
+            collector.turnCollectorOn();
+            hopper.setPushOutPos();
+            hopper.setLiftDownPos();
+            buttonReleased1 = false;
+        }
+
+        if (gamepad1.b && buttonReleased1) {
+            collector.turnCollectorOff();
+            buttonReleased1 = false;
+        }
+
+        // Reverses collector
+        if (gamepad1.x && buttonReleased1) {
+            collector.turnCollectorReverse();
+            buttonReleased1 = false;
+        }
+
+        // Turns launcher on/off
+        if (gamepad2.x && buttonReleased2) {
+            if (launcherOn) {
+                flywheel.setRPM(0);
+                launcherOn = false;
+            } else {
+                flywheel.setRPM(launcherRPM);
+                launcherOn = true;
+            }
+            buttonReleased2 = false;
+        }
+
+        // Adjusts launcher speed every time trigger goes below 0.4
+        if (gamepad2.left_trigger > 0.4 && triggerReleased && launcherOn) {
+            launcherRPM -= 50;
+            flywheel.setRPM(launcherRPM);
+            triggerReleased = false;
+        }
+
+        if (gamepad2.right_trigger > 0.4 && triggerReleased && launcherOn) {
+            launcherRPM += 50;
+            flywheel.setRPM(launcherRPM);
+            triggerReleased = false;
+        }
+
+        // Pushes/retracts collector servo
+        if (gamepad2.y && buttonReleased2) {
+            hopper.setPushInPos();
+            timer.reset();
+            buttonReleased2 = false;
+        }
+
+        if (timer.seconds() > 0.75) {
+            hopper.setPushOutPos();
+        }
+
+        // Lifts/Lowers the collecting platform
+        if (gamepad2.left_bumper && buttonReleased2) {
+            hopper.setLiftDownPos();
+            buttonReleased2 = false;
+        }
+
+        if (gamepad2.right_bumper && buttonReleased2) {
+            collector.turnCollectorOff();
+            hopper.setLiftUpPos();
+            buttonReleased2 = false;
+        }
+
+        // Lifts/Lowers Wobble Goal Arm
+        armPos -= 10 * gamepad2.left_stick_y;
+        if (armPos < wobbleArm.ARM_LOWER_LIMIT) {
+            armPos = wobbleArm.ARM_LOWER_LIMIT;
+        }
+        if (armPos > wobbleArm.ARM_UPPER_LIMIT) {
+            armPos = wobbleArm.ARM_UPPER_LIMIT;
+        }
+
+        // Arm Presets
+        if (gamepad2.dpad_up && buttonReleased2) {
+            armPos = wobbleArm.ARM_POS_LIFT_ARM;
+            buttonReleased2 = false;
+        }
+        if (gamepad2.dpad_down && buttonReleased2) {
+            armPos = wobbleArm.ARM_POS_PICKUP_GOAL;
+            wobbleArm.openClaw();
+            buttonReleased2 = false;
+        }
+        if (gamepad2.dpad_right && buttonReleased2) {
+            armPos = wobbleArm.ARM_POS_OVER_WALL;
+            buttonReleased2 = false;
+        }
+        if (gamepad2.dpad_left && buttonReleased2) {
+            armPos = 0;
+            buttonReleased2 = false;
+        }
+        wobbleArm.setArmPos(armPos);
+
+        // Opens/Closes Wobble Goal Claw
+        if (gamepad2.a && buttonReleased2) {
+            wobbleArm.openClaw();
+            buttonReleased2 = false;
+        }
+        if (gamepad2.b && buttonReleased2) {
+            wobbleArm.closeClaw();
+            buttonReleased2 = false;
+        }
 
 
-                //DPAD-UP - auto drive to general shooting position
-                if (gamepad1.dpad_up) {
-                    collector.turnCollectorOff();
-                    Trajectory driveToShootPositionPath = drive.trajectoryBuilder(currentPose)
-                            .lineToLinearHeading(PoseLibrary.TELE_SHOOTING_POSE.getPose2d())
-                            .build();
 
-                    drive.followTrajectoryAsync(driveToShootPositionPath);
-                    flywheel.setRPM(PoseLibrary.TELE_SHOOTING_POSE.getRPM());
+        // Do not adjust values again until after buttons are released (and pressed again) so the
+        // adjustments are made each time the gamepad buttons are pressed rather than each time through loop
+        if (!gamepad1.a && !gamepad1.b && !gamepad1.x && !gamepad1.y) {
+            buttonReleased1 = true;
+        }
 
-                    currentMode = Mode.LINE_TO_POINT;
-                }
+        if(!gamepad2.left_bumper && !gamepad2.right_bumper && !gamepad2.a && !gamepad2.b && !gamepad2.x && !gamepad2.y && !gamepad2.dpad_up && !gamepad2.dpad_right && !gamepad2.dpad_down && !gamepad2.dpad_left) {
+            buttonReleased2 = true;
+        }
+
+        if (gamepad2.left_trigger < 0.4 && gamepad2.right_trigger < 0.4) {
+            triggerReleased = true;
+        }
 
 
-                //DPAD DOWN - go to 0 degrees heading
-                if (gamepad1.dpad_down) {
-                    angle = 0;
-                    currentMode = Mode.ALIGN_TO_ANGLE;
-                    timer.reset();
-                }
+        //DPAD-UP - auto drive to general shooting position
+        if (gamepad1.dpad_up) {
+            collector.turnCollectorOff();
+            Trajectory driveToShootPositionPath = drive.trajectoryBuilder(currentPose)
+                    .lineToLinearHeading(PoseLibrary.TELE_SHOOTING_POSE.getPose2d())
+                    .build();
 
-                //DPAD LEFT - Shoot Powershots From Right To Left
-                if (gamepad1.dpad_left) {
+            drive.followTrajectoryAsync(driveToShootPositionPath);
+            flywheel.setRPM(PoseLibrary.TELE_SHOOTING_POSE.getRPM());
+
+            currentMode = Mode.LINE_TO_POINT;
+        }
+
+
+        //DPAD DOWN - go to 0 degrees heading
+        if (gamepad1.dpad_down) {
+            angle = 0;
+            currentMode = Mode.ALIGN_TO_ANGLE;
+            timer.reset();
+        }
+
+        //DPAD LEFT - Shoot Powershots From Right To Left
+        if (gamepad1.dpad_left) {
 /*                    POWER_SHOT_POSES = POWER_SHOT_POSES_RIGHT;
                     flywheel.setRPM(POWER_SHOT_POSES[0].getRPM());
                     drive.setPoseEstimate(POWER_SHOT_POSES[0].getPose2d());
                     powerShotState = 0;
                     currentMode = Mode.STAFE_TO_POWERSHOT_POSITION;
  */
-                    currentMode = Mode.ALIGN_TO_ANGLE;
-                }
+            currentMode = Mode.ALIGN_TO_ANGLE;
+        }
 
 
-                //DPAD RIGHT - Shoot Powershots From Left To Right
-                if (gamepad1.dpad_right) {
-                    flywheel.setRPM(2950);
-                    powerShotState = 0;
+        //DPAD RIGHT - Shoot Powershots From Left To Right
+        if (gamepad1.dpad_right) {
+            flywheel.setRPM(2950);
+            powerShotState = 0;
 
-                    Trajectory driveToPowershotPosition = drive.trajectoryBuilder(currentPose)
-                            .strafeRight(63)
-                            .build();
+            Trajectory driveToPowershotPosition = drive.trajectoryBuilder(currentPose)
+                    .strafeRight(63)
+                    .build();
 
-                    drive.followTrajectoryAsync(driveToPowershotPosition);
+            drive.followTrajectoryAsync(driveToPowershotPosition);
 
-                    currentMode = Mode.STAFE_TO_POWERSHOT_POSITION;
-                }
+            currentMode = Mode.STAFE_TO_POWERSHOT_POSITION;
+        }
 
-                //RIGHT BUMPER - Auto Aim
-                if(gamepad1.right_bumper && pipeline.isGoalVisible()) {
-                    collector.turnCollectorOff();
-                    flywheel.setRPM(launcherRPM);
-                    currentMode = Mode.ALIGN_TO_GOAL;
-                    timer.reset();
-                }
-
-
-                //GAMEPAD 2 LEFT STICK BUTTON - Set shooting position
-                if(gamepad2.left_stick_button) {
-                        PoseLibrary.TELE_SHOOTING_POSE.setPose2d(currentPose);
-                }
+        //RIGHT BUMPER - Auto Aim
+        if(gamepad1.right_bumper && pipeline.isGoalVisible()) {
+            collector.turnCollectorOff();
+            flywheel.setRPM(launcherRPM);
+            currentMode = Mode.ALIGN_TO_GOAL;
+            timer.reset();
+        }
 
 
-                //GAMEPAD 2 RIGHT STICK BUTTON - Reset odometry to white line to starting line to maintain good distance
-                if(gamepad2.right_stick_button) {
-                    drive.setPoseEstimate(new Pose2d(12, currentPose.getY(), currentPose.getHeading()));
-                }
+        //GAMEPAD 2 LEFT STICK BUTTON - Set shooting position
+        if(gamepad2.left_stick_button) {
+            PoseLibrary.TELE_SHOOTING_POSE.setPose2d(currentPose);
+        }
 
+
+        //GAMEPAD 2 RIGHT STICK BUTTON - Reset odometry to white line to starting line to maintain good distance
+        if(gamepad2.right_stick_button) {
+            drive.setPoseEstimate(new Pose2d(12, currentPose.getY(), currentPose.getHeading()));
+        }
+
+
+        switch (currentMode){
+            case DRIVER_CONTROL:
                 break;
-
-
 
             case ALIGN_TO_ANGLE:
                 //pass angle in degrees
